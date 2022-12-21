711e035646e58e3d76a17a00b033b1fba19cc198
Watila Costa <watila.costa@redealumni.com>
2022-12-21 16:30:03 -0300

    validation with nil or blank password

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -7,16 +7,14 @@ class AuthController < ApplicationController
   def login
     user = User.find_by_email!(login_params[:email])
 
-    user&.authenticate(login_params[:password])
+    raise JWT::EncodeError, 'Unauthorized with this password.' unless user&.authenticate(login_params[:password])
     token = JwtToken.encode(user_id: user.id)
 
     render json: { token: token }, status: :ok
-  rescue ActiveRecord::RecordNotFound => e
+  rescue ActiveRecord::RecordNotFound, JWT::EncodeError => e
     Rails.logger.error e.message
-    render json: { error: 'User not found!' }, status: :unauthorized
-  rescue JWT::EncodeError => e
-    Rails.logger.error e.message
-    render json: { error: 'Unauthorized!' }, status: :unauthorized
+
+    render json: { error: e.message }, status: :unauthorized
   end
 
   private
